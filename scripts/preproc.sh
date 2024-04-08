d7444a962f3db5f02a29c6c97b9081fa9464d2f5
Lương Tiến Khanh <128344602+khanzil@users.noreply.github.com>
2024-04-08 22:13:39 +0700

    Update preproc.sh

--- scripts/preproc.sh
+++ scripts/preproc.sh
@@ -9,7 +9,7 @@ python seld/main.py -c $CONFIG_FILE preprocess --preproc_mode='extract_data' --d
 python seld/main.py -c $CONFIG_FILE preprocess --preproc_mode='extract_data' --dataset_type='eval'
 
 # Extract scalar
-python seld/main.py -c $CONFIG_FILE preprocess --preproc_mode='extract_scalar' --num_workers=4
+python seld/main.py -c $CONFIG_FILE preprocess --preproc_mode='extract_scalar' --num_workers=8
 
 # Extract meta
 python seld/main.py -c $CONFIG_FILE preprocess --preproc_mode='extract_meta' --dataset_type='dev'
