b250931fc8b56cc7f8669ea58f58b5eb2a611e1c
Lương Tiến Khanh <khanhluongtien@gmail.com>
2024-06-01 08:20:07 +0700

    Update data.py

--- seld/methods/ein_seld/data.py
+++ seld/methods/ein_seld/data.py
@@ -35,7 +35,7 @@ class UserDataset(Dataset):
         self.label_interp_ratio = int(self.label_resolution * self.sample_rate / cfg['data']['hop_length'])
 
         # Chunklen and hoplen and segmentation. Since all of the clips are 60s long, it only segments once here
-        data = np.zeros((1, self.clip_length * self.sample_rate))
+        data = np.zeros((1, self.clip_length * self.sample_rate / cfg['data']['hop_length']))
         if 'train' in self.dataset_type:
             chunklen = int(cfg['data']['train_chunklen_sec'] * self.sample_rate / cfg['data']['hop_length'])     
             hoplen = int(cfg['data']['train_hoplen_sec'] * self.sample_rate / cfg['data']['hop_length'])
@@ -45,7 +45,7 @@ class UserDataset(Dataset):
             hoplen = int(cfg['data']['test_hoplen_sec'] * self.sample_rate / cfg['data']['hop_length'])
             self.segmented_indexes, self.segmented_pad_width = _segment_index(data, chunklen, hoplen, last_frame_always_paddding=True)
         self.num_segments = len(self.segmented_indexes)
-        print(self.segmented_indexes)
+
         # Data and meta path
         fold_str_idx = dataset.fold_str_index
         ov_str_idx = dataset.ov_str_index
