78e8c52317d84fee5dc4b7826d20ddbe91b3ae59
Lương Tiến Khanh <khanhluongtien@gmail.com>
2024-06-05 15:13:32 +0700

    Update DataAug.py

--- seld/methods/ein_seld/data_augmentation/DataAug.py
+++ seld/methods/ein_seld/data_augmentation/DataAug.py
@@ -28,15 +28,16 @@ class SpecAug(nn.Module):
             return x
 
         nb_mels = x.shape[3]
-        for channel in range(x.shape[1]-3):
-            for frame in range (int(x.shape[2]//self._time_mask_step)):
-                time_mask_len = torch.randint(low=0, high=self._time_mask_max_len, size=(1,))[0]
-                time_mask_start = torch.randint(low=0, high=self._time_mask_step - time_mask_len, size=(1,))[0]
-                x[:, channel, self._time_mask_step*frame + time_mask_start: self._time_mask_step*frame + time_mask_start + time_mask_len, :] = np.log(eps)
-    
-                freq_mask_len = torch.randint(low=0, high=self._freq_mask_max_len, size=(1,))[0]
-                freq_mask_start = torch.randint(low=0, high=nb_mels-freq_mask_len, size=(1,))[0]
-                x[:, channel, self._time_mask_step*frame:self._time_mask_step*(frame+1), freq_mask_start:freq_mask_start + freq_mask_len] = np.log(eps)
+        for batch_idx in range(x.shape[0]):
+            for channel in range(x.shape[1]-3):
+                for frame in range (int(x.shape[2]//self._time_mask_step)):
+                    time_mask_len = torch.randint(low=0, high=self._time_mask_max_len, size=(1,))[0]
+                    time_mask_start = torch.randint(low=0, high=self._time_mask_step - time_mask_len, size=(1,))[0]
+                    x[batch_idx, channel, self._time_mask_step*frame + time_mask_start: self._time_mask_step*frame + time_mask_start + time_mask_len, :] = np.log(eps)
+
+                    freq_mask_len = torch.randint(low=0, high=self._freq_mask_max_len, size=(1,))[0]
+                    freq_mask_start = torch.randint(low=0, high=nb_mels-freq_mask_len, size=(1,))[0]
+                    x[batch_idx, channel, self._time_mask_step*frame:self._time_mask_step*(frame+1), freq_mask_start:freq_mask_start + freq_mask_len] = np.log(eps)
     
         return x
 
