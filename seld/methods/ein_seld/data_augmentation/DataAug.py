70522baec4c94c66b0c1931ebf21b066251974e7
Lương Tiến Khanh <khanhluongtien@gmail.com>
2024-06-06 12:43:47 +0700

    Update DataAug.py

--- seld/methods/ein_seld/data_augmentation/DataAug.py
+++ seld/methods/ein_seld/data_augmentation/DataAug.py
@@ -1,18 +1,21 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import torchaudio.transforms as transforms
 eps = torch.finfo(float).eps
 torch.manual_seed(2)
 np.random.seed(2)
 cfg = {}
 
 class SpecAug(nn.Module):
-    def __init__(self, time_mask_max_len=10, time_mask_step=40, freq_mask_max_len=20, p=1):
+    def __init__(self, time_mask_max_len=15, time_mask_num=2, freq_mask_max_len=20, freq_mask_num=2, p=1):
         super().__init__()
         self._time_mask_max_len = time_mask_max_len
-        self._time_mask_step = time_mask_step
+        self._time_mask_num = time_mask_num
 
         self._freq_mask_max_len = freq_mask_max_len
+        self._freq_mask_num = freq_mask_num
+
         self._p = p
 
         self.requires_grad_ = False
@@ -26,23 +29,13 @@ class SpecAug(nn.Module):
         '''
         if np.random.rand() > self._p:
             return x
+        transform = transforms.SpecAugment(n_freq_masks=self._freq_mask_num, freq_mask_param=self._freq_mask_max_len,\
+                                           n_time_masks=self._time_mask_num, time_mask_param=self._time_mask_max_len, p=0.5)
 
-        nb_mels = x.shape[3]
-        for batch_idx in range(x.shape[0]):
-            for channel in range(x.shape[1]-3):
-                for frame in range (int(x.shape[2]//self._time_mask_step)):
-                    time_mask_len = torch.randint(low=0, high=self._time_mask_max_len, size=(1,))[0]
-                    time_mask_start = torch.randint(low=0, high=self._time_mask_step - time_mask_len, size=(1,))[0]
-                    x[batch_idx, channel, self._time_mask_step*frame + time_mask_start: self._time_mask_step*frame + time_mask_start + time_mask_len, :] = np.log(eps)
-
-                    freq_mask_len = torch.randint(low=0, high=self._freq_mask_max_len, size=(1,))[0]
-                    freq_mask_start = torch.randint(low=0, high=nb_mels-freq_mask_len, size=(1,))[0]
-                    x[batch_idx, channel, self._time_mask_step*frame:self._time_mask_step*(frame+1), freq_mask_start:freq_mask_start + freq_mask_len] = np.log(eps)
-    
-        return x
+        return transform(x)
 
 class RandomCutoff(nn.Module):
-    def __init__(self, time_mask_max_len=10, time_mask_step=40, freq_mask_max_len=20, p=0.3):
+    def __init__(self, time_mask_max_len=10, time_mask_step=40, freq_mask_max_len=35, p=0.3):
         super().__init__()
         self._time_mask_step = time_mask_step
         self._time_mask_max_len = time_mask_max_len
